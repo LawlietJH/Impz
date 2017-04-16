3140a9be9b22f652b0e05e60d53ebec480c04b76
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-04-16 15:33:26 -0500

    Disminuida la indentación
    Se Disminuyeron los niveles de indentación en la impresion en pantalla
    de la Información

--- Info.py
+++ Info.py
@@ -6,7 +6,7 @@ import os
 
 
 os.system("cls && Title Impz.py                "+\
-		"By: LawlietJH                v1.0.7    ")
+		"By: LawlietJH                v1.1.1    ")
 
 
 
@@ -22,42 +22,42 @@ def Info():
 	print("""
 	
 	
-		[!] ESSID = Nombre De Red.
-		[!] BSSID = Dirección MAC.
-		[!] Key WPA = Contraseña De La Red. 
+	[!] ESSID = Nombre De Red.
+	[!] BSSID = Dirección MAC.
+	[!] Key WPA = Contraseña De La Red. 
 	
 	
-	[~] Información:
+  [~] Información:
+	
+	
+	[+] Las Contraseñas de estas Redes siempre son Números.
+	[+] Siempre tienen una Contraseña de 9 Digitos.
+	[+] Siempre Comienzan Con el Número 2.
+	[+] El Segundo Digito Normalmente Es el 7.
+	
+		[*] El segundo Digito varia entre el 6 y el 8.
+	
+	[+] El Primer Diccionario recomendado Probar es el Impz-27[0 - 9999999].ZioN.
+	
+		[*] La Mayoria De Las Redes Cisco Pegatron empiezan con 27.
+	
+	[+] Las Redes 'Cisco Pegatron' Se Ven De La Siguiente Manera:
 		
-		
-		[+] Las Contraseñas de estas Redes siempre son Números.
-		[+] Siempre tienen una Contraseña de 9 Digitos.
-		[+] Siempre Comienzan Con el Número 2.
-		[+] El Segundo Digito Normalmente Es el 7.
-		
-			[*] El segundo Digito varia entre el 6 y el 8.
-		
-		[+] El Primer Diccionario recomendado Probar es el Impz-27[0 - 9999999].ZioN.
-		
-			[*] La Mayoria De Las Redes Cisco Pegatron empiezan con 27.
-		
-		[+] Las Redes 'Cisco Pegatron' Se Ven De La Siguiente Manera:
+		[*] Ejemplo de ESSID, BSSID y Key WPA:
 			
-			[*] Ejemplo de ESSID, BSSID y Key WPA:
-				
-				[-] ~ ESSID = 1bf0a2
-				[-] ~ BSSID = 00:71:C2:21:FE:A8
-				[-] Key WPA = 274345388
-				
-			[*] Ejemplos de ESSID's:
-				
-				[-] c3f2ba
-				[-] e922e8
-				[-] aad92a
-				[-] ab0cb8
-				[-] b5967a
-				    etc...
+			[-] ~ ESSID = 1bf0a2
+			[-] ~ BSSID = 00:71:C2:21:FE:A8
+			[-] Key WPA = 274345388
 			
+		[*] Ejemplos de ESSID's:
+			
+			[-] c3f2ba
+			[-] e922e8
+			[-] aad92a
+			[-] ab0cb8
+			[-] b5967a
+			    etc...
+		
 	""")
 
 
