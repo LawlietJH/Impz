84bcdb1b283d1dc3feb599210f31a90f99adfad4
Lawliet <jerry_howl_lawliet@hotmail.com>
2017-04-16 13:21:34 -0500

    Datos Añadidos
    Se Mejoró La Precisión Sobre El Tamaño De Salida Del Archivo

--- Impz.py
+++ Impz.py
@@ -9,7 +9,7 @@
 #                       ██║██║ ╚═╝ ██║██║     ███████╗
 #                       ╚═╝╚═╝     ╚═╝╚═╝     ╚══════╝
 #                                                         By: LawlietJH
-#                                                               v1.0.3
+#                                                               v1.0.4
 # Fuente: 'ANSI Shadow' - Desde: http://patorjk.com/software/taag/#p=display&f=ANSI%20Shadow&t=Impz
 
 import time
@@ -19,24 +19,26 @@ import os
 
 
 Autor = "LawlietJH"
-Version = "v1.0.3"
+Version = "v1.0.4"
 
 
 
 def Imp_27(Minim=0):
 	
-	os.system("cls && Title Creando Diccionario: Impz-27.ZioN.")
-	
 	Total = 10000000
-
+	CantCad = Total - Minim
+	Tam = ((Total-Minim) / 100000) * 1.04
+	
+	os.system("cls && Title Creando Diccionario: Impz-27["+str(Minim)+" - "+str(Total-1)+"].ZioN")
+	
 	open("Impz-27["+str(Minim)+" - "+str(Total-1)+"].ZioN","a")
 	Eny = open("Impz-27["+str(Minim)+" - "+str(Total-1)+"].ZioN","w")
 	
 	print("\n\n\n\t [+] Creando Diccionario. 99% Efectivo.")
 	print("\n\t    [~] Para Hackear Redes: 'Cisco Pegatron'.")
 	print("\n\t    [~] Archivo de Salida:  'Impz-27["+str(Minim)+" - "+str(Total-1)+"].ZioN'")
-	print("\n\t    [~] Tamaño Salida Aprox:", (Total-Minim) // 100000, "Mb")
-	print("\n\t    [~] Cantidad de Cadenas:", Total-Minim, "\n\n\n\n")
+	print("\n\t    [~] Tamaño Salida Aprox:", Tam, "Mb")
+	print("\n\t    [~] Cantidad de Cadenas:", CantCad, "\n\n\n\n")
 	
 	for x in range(Minim, Total):
 				
@@ -68,13 +70,17 @@ def Impz():
 		   >>> """, end="")
 		
 		try:
+			
 			Resp = int(input(""))
+			
 			if Resp == 1:
+				
 				while True:
 					
 					os.system("cls && Title Creación De Diccionario Con Prefijo 27. Max[10,000,000]")
 					
 					try:
+						
 						print("\n\n\t Creación De Diccionario Con Prefijo 27. Max[10,000,000]"+\
 							  "\n\n\n\t [1] Iniciar En Algun Punto."+\
 							  "\n\t [2] Iniciar Normal [Desde 0 - 9,999,999]."+\
@@ -84,6 +90,7 @@ def Impz():
 						Resp2 = int(input(""))
 						
 						if Resp2 == 1:
+							
 							try:
 								
 								Minim = int(input("\n\n\t Elige Un Número De Inicio [ 0 - 10,000,000 ]: "))
@@ -93,10 +100,12 @@ def Impz():
 									print("\n\n\t Número Inválido. Se Usara El 0 por Defecto.")
 									time.sleep(1.5)
 									Imp_27()
+									
 							except KeyboardInterrupt:
 								print("\n\n\t Cancelando...")
 								time.sleep(1.5)
 								break
+								
 							except:
 									print("\n\n\t Opción Inválida. Se Usara El 0 por Defecto.")
 									time.sleep(1.5)
@@ -112,7 +121,6 @@ def Impz():
 							time.sleep(1.5)
 						
 					except KeyboardInterrupt:
-						
 						print("\n\n\t Cancelando...")
 						time.sleep(1.5)
 						return
@@ -126,17 +134,16 @@ def Impz():
 			else:
 				print("\n\n\n\t Elige Una Opción Correcta.")
 				time.sleep(1.5)
+				
 		except KeyboardInterrupt:
 			print("\n\n\t Saliendo...")
 			time.sleep(1.5)
 			exit(1)
+			
 		except:
 			print("\n\n\n\t Elige Una Opción Correcta.")
 			time.sleep(1.5)
-		
-		
-	
-	
+
 
 
 def Progreso(x, Total):
@@ -158,6 +165,7 @@ def Progreso(x, Total):
 
 
 def Tiempo(sec):
+	
 	if sec >= 3600:  # Convierte a Horas
 		return '{0:d} hora(s)'.format(int(sec / 3600))
 	elif sec >= 60:  # Convierte a Minutos
